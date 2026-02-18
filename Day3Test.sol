f1fa7239c3d04898130d361b3b9fe1e9a57b6681
Johnson Oyemade <johnsonoca@gmail.com>
2026-02-18 20:44:34 +0100

    MAde corrections to class test erc and ether saving contract

--- Day3Test.sol
+++ Day3Test.sol
@@ -21,31 +21,45 @@ contract SavingsContract {
 
     function depositEth() external payable {
         require(msg.value > 0, "Must send some money");
+
         ethBalance[msg.sender] += msg.value;
+        
         emit MoneyDeposited(msg.sender, msg.value);
     }
 
     function depositToken(uint256 amount) external {
         require(amount > 0, "Amount must be positive");
+        
         bool success = IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        
         require(success, "Token transfer failed");
+        
         tokenBalance[msg.sender] += amount;
+        
         emit MoneyDeposited(msg.sender, amount);
     }
 
     function withdrawEth(uint256 amount) external {
         require(ethBalance[msg.sender] >= amount, "Not enough balance");
+        
         ethBalance[msg.sender] -= amount;
+        
         (bool sent, ) = msg.sender.call{value: amount}("");
+        
         require(sent, "Failed to send");
+     
         emit MoneyWithdrawn(msg.sender, amount);
     }
 
     function withdrawToken(uint256 amount) external {
         require(tokenBalance[msg.sender] >= amount, "Not enough balance");
+     
         tokenBalance[msg.sender] -= amount;
+     
         bool success = IERC20(tokenAddress).transfer(msg.sender, amount);
+     
         require(success, "Token transfer failed");
+     
         emit MoneyWithdrawn(msg.sender, amount);
     }
 }
